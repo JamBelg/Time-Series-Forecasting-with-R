cb9b7d8d9757fc7fbcf1c1f4a4089de079352a11
JamBelg <95283203+JamBelg@users.noreply.github.com>
2022-09-04 20:31:03 +0200

    Add files via upload

--- Code_v1.R
+++ Code_v1.R
@@ -1,8 +1,8 @@
 # Remove objects
 rm(list=ls())
-# setwd("/Users/Shared/Dev Jamel/Store Sales - Time Series Forecasting")
+setwd("/Users/Shared/Dev Jamel/Store Sales - Time Series Forecasting")
 # setwd("C:/tmp/Store Sales - Time Series Forecasting")
-setwd("U:/R scripts/Project_Directory/Kaggle/Store Sales - Time Series Forecasting")
+# setwd("U:/R scripts/Project_Directory/Kaggle/Store Sales - Time Series Forecasting")
 
 # Load libraries
 library(dplyr)
@@ -201,9 +201,22 @@ plot_salesvsoil <-df_train %>%
 ggsave("pics/plot_oil.png")
 
 
+# Promotions
+Plot_promotions <- df_train %>%
+  group_by(date) %>%
+  summarise(
+    sales=mean(sales,na.rm=TRUE),
+    onprom=sum(onpromotion,na.rm=TRUE)
+  ) %>%
+  mutate(Promotions=ifelse(onprom==0,"No","Yes")) %>%
+  ggplot(aes(x=Promotions,y=sales,fill=Promotions))+geom_boxplot()+
+  labs("Influence of promotions on daily sales",subtitle="Ecuador (2013-2017)")+
+  xlab("Promotions ?")+ylab("Daily sales")
+ggsave("pics/plot_promotions.png")
 
 
-
+df_oil %>%
+  ggplot(aes(x=date,y=oil_NNA,groups=1)) + geom_line()
 
 
 # Seasonal decomposition
@@ -232,7 +245,6 @@ plot(stl(dat_ts,s.window = "periodic"),
      main="Seasonal Decomposition of Time Series by Loess")
 dev.off()
 
-
 # Augmented Dickey-Fuller(ADF) Test
 # p-value <0.05 -> data is "stationary"
 print(adf.test(dat_ts))
@@ -253,7 +265,22 @@ data<- df_train %>%
     value=mean(sales,na.rm=TRUE)
   )
 
-
+# Store_nbr 51, family MEATS
+data_1<- df_train %>%
+  filter(!grepl("Terremoto", description, fixed = TRUE)) %>%
+  filter(store_nbr==51,family=="MEATS") %>%
+  group_by(date) %>%
+  summarise(
+    value=mean(sales,na.rm=TRUE)
+  )
+# Store_nbr 51, family PRODUCE
+data_2<- df_train %>%
+  filter(!grepl("Terremoto", description, fixed = TRUE)) %>%
+  filter(store_nbr==51,family=="PRODUCE") %>%
+  group_by(date) %>%
+  summarise(
+    value=mean(sales,na.rm=TRUE)
+  )
 
 # split data in two
 # last 3 months for validation
@@ -269,15 +296,15 @@ splits %>%
 
 # Automatic Models
 # 1- Auto Arima
-model_fit_arima <- arima_reg() %>%
+model_arima <- arima_reg() %>%
   set_engine("auto_arima") %>%
   fit(value ~ date, training(splits))
-model_fit_arima
+model_arima
 
 
 
 # 2- Prophet
-model_fit_prophet <- prophet_reg(seasonality_yearly = TRUE,seasonality_weekly =TRUE) %>%
+model_prophet <- prophet_reg(seasonality_yearly = TRUE,seasonality_weekly =TRUE) %>%
   set_engine("prophet",holidays=df_holidays) %>%
   fit(value ~ date, training(splits))
 
@@ -285,17 +312,15 @@ model_fit_prophet <- prophet_reg(seasonality_yearly = TRUE,seasonality_weekly =T
 
 
 # 3- TBATS
-model_fit_tbats<-seasonal_reg(mode="regression",
-                              seasonal_period_1= "auto",
-                              seasonal_period_2= "1 year",
-                              seasonal_period_3= "1 month") %>%
+model_tbats<-seasonal_reg(mode="regression",
+                              seasonal_period_1= "auto") %>%
   set_engine("tbats") %>%
   fit(value ~ date, training(splits))
 
 
 
 # 4- Seasonal Naive
-model_fit_snaive <- naive_reg(seasonal_period="1 year") %>%
+model_snaive <- naive_reg() %>%
   set_engine("snaive") %>%
   fit(value ~ date, training(splits))
 
@@ -304,23 +329,26 @@ model_fit_snaive <- naive_reg(seasonal_period="1 year") %>%
 
 # Machine learning models
 # make a recipe
-recipe_spec <- recipe(value ~ date, training(splits)) %>%
+recipe <- recipe(value ~ date, training(splits)) %>%
   step_timeseries_signature(date) %>%
   step_fourier(date, period = c(365, 91.25, 30.42), K = 5) %>%
   step_dummy(all_nominal(),all_predictors())
 
-recipe_spec %>% prep() %>% juice()
+recipe %>% 
+  prep() %>%
+  juice()
+
 
 # 5- Elastic Net
 library(glmnet)
 # 5.1 Set Engine
-model_spec_glmnet <- linear_reg(penalty = 0.01, mixture = 0.5) %>%
+engine_glmnet <- linear_reg(penalty = 0.01, mixture = 0.5) %>%
   set_engine("glmnet")
 
 # 5.2 Fit the workflow
-workflow_fit_glmnet <- workflow() %>%
-  add_model(model_spec_glmnet) %>%
-  add_recipe(recipe_spec %>% step_rm(date)) %>%
+model_glmnet <- workflow() %>%
+  add_model(engine_glmnet) %>%
+  add_recipe(recipe %>% step_rm(date)) %>%
   fit(training(splits))
 
 
@@ -328,72 +356,65 @@ workflow_fit_glmnet <- workflow() %>%
 # 6- Random forest
 library(randomForest)
 # 6.1 Set engine
-model_spec_rf <- rand_forest(mode="regression",trees = 500, min_n = 50) %>%
+engine_rf <- rand_forest(mode="regression",trees = 50, min_n = 5) %>%
   set_engine("randomForest")
 
 # 6.2 Fit workflow
-workflow_fit_rf <- workflow() %>%
-  add_model(model_spec_rf) %>%
-  add_recipe(recipe_spec %>% step_rm(date)) %>%
+model_rf <- workflow() %>%
+  add_model(engine_rf) %>%
+  add_recipe(recipe %>% step_rm(date)) %>%
   fit(training(splits))
 
 
 # Hybrid ML
 # 7 Prophet boost
-model_spec_prophet_boost <- prophet_boost() %>%
+engine_prophet_boost <- prophet_boost() %>%
   set_engine("prophet_xgboost") 
-workflow_fit_prophet_boost <- workflow() %>%
-  add_model(model_spec_prophet_boost) %>%
-  add_recipe(recipe_spec) %>%
+model_prophet_boost <- workflow() %>%
+  add_model(engine_prophet_boost) %>%
+  add_recipe(recipe) %>%
   fit(training(splits))
 
-workflow_fit_prophet_boost
 
 
 
 # Modeltime table
-model_table <- modeltime_table(
-  model_fit_arima,
-  model_fit_prophet,
-  model_fit_tbats,
-  model_fit_snaive,
-  workflow_fit_glmnet,
-  workflow_fit_rf,
-  workflow_fit_prophet_boost
+models_table <- modeltime_table(
+  model_arima,
+  model_prophet,
+  model_tbats,
+  model_snaive,
+  model_glmnet,
+  model_rf,
+  model_prophet_boost
 ) 
 
 
-# model_table %>%
-#   modeltime_calibrate(new_data=testing(splits)) %>%
-#   modeltime_forecast(
-#     new_data=testing(splits),
-#     actual_data=df_train
-#   ) %>%
-#   plot_modeltime_forecast(.y_lab="Sales",.x_lab="Date",.title="Sales forecasting",
-#                           .interactive = TRUE,.smooth=FALSE)
-
-calibration_table <- model_table %>%
+# Calibrate models for accuracy and forecast
+calibration_table <- models_table %>%
   modeltime_calibrate(testing(splits))
-calibration_table %>%
-  modeltime_forecast(actual_data = tail(data,120)) %>%
-  plot_modeltime_forecast(.interactive = FALSE,.conf_interval_show = FALSE)
-
-
-
-# Calibration table
-calibration_table <- model_table %>%
-  modeltime_calibrate(testing(splits))
-
-
-calibration_table %>%
-  modeltime_forecast(actual_data = data) %>%
-  plot_modeltime_forecast(.interactive = TRUE,.smooth=FALSE)
 
 # Models accuracy
 calibration_table %>%
   modeltime_accuracy() %>%
   arrange(desc(rsq)) %>%
   table_modeltime_accuracy(.interactive = FALSE)
+
+
+# Plot prohet + prophet+boost
+calibration_table %>%
+  filter(.model_id %in% c(2,7)) %>%
+  modeltime_forecast(actual_data = tail(data,120)) %>%
+  plot_modeltime_forecast(.interactive = FALSE,.conf_interval_show = FALSE)
+
+
+
+# Plot all models
+calibration_table %>%
+  modeltime_forecast(actual_data = data) %>%
+  plot_modeltime_forecast(.interactive = TRUE,.smooth=FALSE)
+
+
 
 
 # 3 months prediction
@@ -403,9 +424,10 @@ calibration_table %>%
   
   # Refit and Forecast Forward
   modeltime_refit(data) %>%
-  modeltime_forecast(h = "12 month", actual_data = tail(data,600)) %>%
-  plot_modeltime_forecast(.y_lab="Sales",.x_lab="Date",.title="Sales forecasting",
-                          .interactive = FALSE,.smooth=TRUE)
+  modeltime_forecast(h = "3 month", actual_data = tail(data,600)) %>%
+  plot_modeltime_forecast(.y_lab="Sales",.x_lab="Date",
+                          .title="Sales forecasting-Sales_nbr 51-PRODUCE product",
+                          .interactive = FALSE,.smooth=FALSE)
 
 
 
